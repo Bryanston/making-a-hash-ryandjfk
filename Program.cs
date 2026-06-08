1e1a3860f1ddd1785ddd9f74d402c0e9d261e108
ryandjfk <ma_r@bryanston.co.uk>
2026-06-08 08:39:47 +0000

    1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,35 @@ class Program
     {
         // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+
+        Console.Write("Enter key: ");
+        string userKey = Console.ReadLine();
+
+        Console.Write("Enter limit: ");
+        int limit = Convert.ToInt32(Console.ReadLine());
+
+        int total = 0;
+        foreach(char c in userKey)
+        {
+            total += (int)c;
+        }
+
+        total = total * total;
+
+        Console.WriteLine(total);
+        string numString = Convert.ToString(total);
+        
+        int middle = numString.Length / 2;
+        Console.WriteLine(middle);
+
+        string middleTwo = numString.Substring(middle, 2);
+        
+        int numMiddleTwo = Convert.ToInt32(middleTwo);
+
+        int hashValue = numMiddleTwo % limit;
+
+        Console.WriteLine($"Hash value is {hashValue}.");
+        
+        
     }
 }
